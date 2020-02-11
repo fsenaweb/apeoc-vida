f0be793e27aa21c41160407f80983cd987820135
Matheus Ricelly <fsenaweb@gmail.com>
2020-02-11 15:15:00 -0300

    Mudança no componente telefone

--- src/components/utils/Phone.vue
+++ src/components/utils/Phone.vue
@@ -2,7 +2,7 @@
     <div class="mt-4" :class="color">
         <p class="h4 display-5 text-center p-0 m-0">Horário de Atendimento</p>
         <p class="h6 display-5 text-center p-0 m-0">De Segunda à Sexta das 8:00 às 18:00hs</p>
-        <p class="h3 display-5 text-center font-weight-bolder p-0 m-1">0800 726 0638</p>
+        <p class="h3 display-5 text-center font-weight-bolder p-0 m-1">000 000 0000</p>
     </div>
 </template>
 
