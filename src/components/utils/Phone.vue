48b18a6f85de3bbea8d8b48a1dd0963ff046c719
Matheus Ricelly <fsenaweb@gmail.com>
2022-09-02 21:20:45 -0300

    fix: correções no layout e ajustes no docker

--- src/components/utils/Phone.vue
+++ src/components/utils/Phone.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class="mt-4" :class="color">
-        <p class="h4 display-5 text-center p-0 m-0">Horário de Atendimento</p>
-        <p class="h6 display-5 text-center p-0 m-0">De Segunda à Sexta das 8:00 às 18:00hs</p>
-        <p class="h3 display-5 text-center font-weight-bolder p-0 m-1">0800 887 0576</p>
-    </div>
+  <div class="mt-4" :class="color">
+    <p class="h4 display-5 text-center p-0 m-0">Horário de Atendimento</p>
+    <p class="h6 display-5 text-center p-0 m-0">De Segunda à Sexta das 8:00 às 18:00hs</p>
+    <p class="h3 display-5 text-center font-weight-bolder p-0 m-1">
+      <img src="./../../assets/images/logo-whats.png" alt="Atendimento Whatsapp" class="logo-whats"/>(85) 9.8132-4670
+    </p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Phone',
-        props: {
-            color: {
-                type: String,
-                required: false,
-                default: 'text-black'
-            }
-        }
+export default {
+  name: 'Phone',
+  props: {
+    color: {
+      type: String,
+      required: false,
+      default: 'text-black'
     }
+  }
+}
 </script>
 
 <style scoped>
-
+.logo-whats {
+  padding-right: 10px;
+}
 </style>
